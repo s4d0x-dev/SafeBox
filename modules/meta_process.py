a768b0609e6a771ed686355b367c7ed2b0e0534a
S4D0X <faredba@outlook.com>
2025-05-18 13:15:11 +0430

    Added base64 encoding to the metadata

--- modules/meta_process.py
+++ modules/meta_process.py
@@ -1,21 +1,25 @@
-# import base64 - for the next update
+import base64
 from typing import Tuple
 
 def attach_metadata(file_stream: bytes, file_name: str) -> bytes:
     """
     Append metadata with the original filename to encrypted binary data.
     """
-    metadata = f'\n==b-ssb_metadata="{file_name}"'.encode("utf-8")
+    encoded_name = base64.b64encode(file_name.encode("utf-8")).decode("ascii")
+    metadata = f'\n==b-ssb_metadata=[{encoded_name}]'.encode("utf-8")
     return file_stream + metadata
 
 def extract_metadata(file_in: bytes) -> Tuple[bytes, str]:
     """
-    Splits encrypted data and metadata from input file(encrypted) data. 
+    Splits encrypted data and metadata from input file (encrypted) data. 
     """
-    metadata = b'\n==b-ssb_metadata="'
+    metadata = b'\n==b-ssb_metadata=['
     if metadata not in file_in:
         raise ValueError("The SafeBox metadata not found in the file.")
     
     encrypted_data, meta_part = file_in.split(metadata, 1)
-    original_filename = meta_part.decode(errors="ignore").split('"', 1)[0].strip()
+    end_index = meta_part.find(b']')
+    original_filename_encoded_bytes = meta_part[:end_index]
+    original_filename = base64.b64decode(original_filename_encoded_bytes).decode("utf-8")
+    
     return encrypted_data, original_filename
