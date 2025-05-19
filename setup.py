12a1289ee9d2eacd87b98ad35796513a661b50a1
S4D0X <faredba@outlook.com>
2025-05-19 15:22:54 +0430

    Some bugs fixed.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name="SafeBox",
-    version="1.1.0",
+    version="1.1.1",
     description="Secure file locker by S4D0X",
     executables=[Executable("safebox.py")],
 )
