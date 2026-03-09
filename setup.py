1646a3ea63c914b0e4decb75d9dc1152811425c5
S4D0X <faredba@outlook.com>
2026-03-09 13:38:49 +0430

    Version code fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name="SafeBox",
-    version="1.2.0",
+    version="1.2.1",
     description="Secure file locker by S4D0X",
     executables=[Executable("safebox.py")],
 )
